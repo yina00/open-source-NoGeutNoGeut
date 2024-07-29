6ff98c32b9807a59a3ab6e68573dad3840460ff2
yeonju000 <20221146@sungshin.ac.kr>
2024-07-29 12:07:46 +0000

    알림봇 확인

--- main.js
+++ main.js
@@ -3,18 +3,14 @@ const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-// EJS 설정
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Express-EJS-Layouts 미들웨어 설정
 app.use(expressLayouts);
-app.set('layout', 'layouts/layout'); // 기본 레이아웃 파일 설정
+app.set('layout', 'layouts/layout');
 
-// 정적 파일 제공 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 라우트 설정
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -26,7 +22,6 @@ app.get('/category', (req, res) => {
   
   
 
-// 서버 시작
 const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
